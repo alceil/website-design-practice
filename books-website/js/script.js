322b221c9bddf727376b3e843ad8315595aefbf4
alceil <ashishshajisrampickal@gmail.com>
2021-10-22 11:05:46 +0530

    added icons

--- books-website/js/script.js
+++ books-website/js/script.js
@@ -1,3 +1,18 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+  }
+
+
 window.onscroll = () =>{
     if(window.scrollY>80)
     {
